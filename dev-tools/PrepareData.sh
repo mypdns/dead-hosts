e6adfcf5cd8e3678aaa64d8b124fec581883fc6a
spirillen <44526987+spirillen@users.noreply.github.com>
2020-01-22 15:05:40 +0100

    Double quote to prevent globbing and word splitting

--- dev-tools/PrepareData.sh
+++ dev-tools/PrepareData.sh
@@ -9,7 +9,7 @@
 # Setup input bots and referer lists
 # **********************************
 
-input1=${TRAVIS_BUILD_DIR}/PULL_REQUESTS/domains.txt
+input1="${TRAVIS_BUILD_DIR}/PULL_REQUESTS/domains.txt"
 
 # *********************************************
 # Get Travis CI Prepared for Committing to Repo
@@ -29,13 +29,13 @@ input1=${TRAVIS_BUILD_DIR}/PULL_REQUESTS/domains.txt
 # Preparing to export directly from the Database hosting our 
 # rpz.mypdns.cloud zone records for testing
 
-mysql --batch --raw -u$DB_USER -p$DB_PASS -h$DB_HOST -D $DBASE\
-    -e'SELECT `name` FROM $DB_TABLE WHERE `domain_id`="$domain_id" AND name NOT REGEXP "^[*]\.";' \
+mysql --batch --raw -u"${DB_USER}" -p"${DB_PASS}" -h"${DB_HOST}" -D $DBASE\
+    -e''SELECT `name` FROM $DB_TABLE WHERE `domain_id`="$domain_id" AND name NOT REGEXP "^[*]\.";'' \
     | sed 's/\.mypdns\.cloud//;/\.mypdns\.cloud/d;/^name$/d' > "${input1}"
 
 printf "\n\n\nCount number of records exported\n\n\n"
 
-printf "\n\n\nThe test file contains $(wc -l '${input1}')\n\n\n"
+printf "\n\n\nThe test file contains $(wc -l "${input1}")\n\n\n"
 
 # **************************************************************************
 # Sort lists alphabetically and remove duplicates before cleaning Dead Hosts
