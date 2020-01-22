faf06ec71045f8acd27b5421c5b0f9024670d3b2
The Unknown <5105020+AnonymousPoster@users.noreply.github.com>
2020-01-22 14:51:05 +0100

    [ci skip]
    Preparing to export directly from the Database hosting our rpz.mypdns.cloud zone records for testing

--- dev-tools/PrepareData.sh
+++ dev-tools/PrepareData.sh
@@ -15,16 +15,23 @@ input1=${TRAVIS_BUILD_DIR}/PULL_REQUESTS/domains.txt
 # Get Travis CI Prepared for Committing to Repo
 # *********************************************
 
-PrepareTravis () {
-    git remote rm origin
-    git remote add origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
-    git config --global user.email "${GIT_EMAIL}"
-    git config --global user.name "${GIT_NAME}"
-    git config --global push.default simple
-    git checkout "${GIT_BRANCH}"
-    ulimit -u
-}
-PrepareTravis
+#PrepareTravis () {
+#    git remote rm origin
+#    git remote add origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
+#    git config --global user.email "${GIT_EMAIL}"
+#    git config --global user.name "${GIT_NAME}"
+#    git config --global push.default simple
+#    git checkout "${GIT_BRANCH}"
+#    ulimit -u
+#}
+#PrepareTravis
+
+# Preparing to export directly from the Database hosting our 
+# rpz.mypdns.cloud zone records for testing
+
+#mysql --batch --raw -u$DB_USER -p$DB_PASS -h$DB_HOST -D $DBASE\
+#    -e'SELECT `name` FROM $DB_TABLE WHERE `domain_id`="$domain_id" AND name NOT REGEXP "^[*]\.";' \
+#    | sed 's/\.mypdns\.cloud//;/\.mypdns\.cloud/d' > ${input1}
 
 # **************************************************************************
 # Sort lists alphabetically and remove duplicates before cleaning Dead Hosts
