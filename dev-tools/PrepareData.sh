ab3e7e6c65749e3ea7cb8b9eba2bb2bdb3d6e7d3
The Unknown <5105020+AnonymousPoster@users.noreply.github.com>
2020-01-22 14:56:59 +0100

    Update PrepareData.sh

--- dev-tools/PrepareData.sh
+++ dev-tools/PrepareData.sh
@@ -29,9 +29,13 @@ input1=${TRAVIS_BUILD_DIR}/PULL_REQUESTS/domains.txt
 # Preparing to export directly from the Database hosting our 
 # rpz.mypdns.cloud zone records for testing
 
-#mysql --batch --raw -u$DB_USER -p$DB_PASS -h$DB_HOST -D $DBASE\
-#    -e'SELECT `name` FROM $DB_TABLE WHERE `domain_id`="$domain_id" AND name NOT REGEXP "^[*]\.";' \
-#    | sed 's/\.mypdns\.cloud//;/\.mypdns\.cloud/d' > ${input1}
+mysql --batch --raw -u$DB_USER -p$DB_PASS -h$DB_HOST -D $DBASE\
+    -e'SELECT `name` FROM $DB_TABLE WHERE `domain_id`="$domain_id" AND name NOT REGEXP "^[*]\.";' \
+    | sed 's/\.mypdns\.cloud//;/\.mypdns\.cloud/d;/^name$/d' > "${input1}"
+
+printf "\n\n\nCount number of records exported\n\n\n"
+
+printf "\n\n\nThe test file contains $(wc -l '${input1}')\n\n\n"
 
 # **************************************************************************
 # Sort lists alphabetically and remove duplicates before cleaning Dead Hosts
