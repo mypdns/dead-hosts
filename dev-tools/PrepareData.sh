26942ac39f3a9f08a7be677138bb8d04696ef9c7
spirillen <44526987+spirillen@users.noreply.github.com>
2020-01-22 17:10:57 +0100

    Update PrepareData.sh

--- dev-tools/PrepareData.sh
+++ dev-tools/PrepareData.sh
@@ -29,8 +29,8 @@ input1="${TRAVIS_BUILD_DIR}/PULL_REQUESTS/domains.txt"
 # Preparing to export directly from the Database hosting our 
 # rpz.mypdns.cloud zone records for testing
 
-mysql --batch --raw --host="${DB_HOST}" --user="${DB_USER}" --password="${DB_PASS}" --database="${DBASE}" -N \
-    -q -e'SELECT `name` FROM $DB_TABLE WHERE `domain_id`="$domain_id" AND name NOT REGEXP "^[*]\.";' \
+mysql --batch --raw --host="${RPZ_DB_SERVER}" --user="${RPZ_DB_USER}" --password="${RPZ_DB_PASS}" --database="${RPZ_DB}" -N \
+    -q -e'SELECT `name` FROM "$RPZ_DB_TABLE" WHERE `domain_id`="$RPZ_DOMAIN_ID" AND name NOT REGEXP "^[*]\.";' \
     | sed 's/\.mypdns\.cloud//;/\.mypdns\.cloud/d;/^name$/d' > "${input1}"
 
 printf "\n\n\nCount number of records exported\n\n\n"
