2a3ccb35d7e51a6326e22c46c4540a6f195242df
spirillen <44526987+spirillen@users.noreply.github.com>
2020-01-25 13:17:43 +0100

    Update DataTesting.sh

--- dev-tools/DataTesting.sh
+++ dev-tools/DataTesting.sh
@@ -5,23 +5,7 @@
 # Created by: Mitchell Krog (mitchellkrog@gmail.com)
 # Copyright: Mitchell Krog - https://github.com/mitchellkrogza
 
-# ****************************************************************
-# This uses the awesome PyFunceble script created by Nissar Chababy
-# Find funceble at: https://github.com/funilrys/PyFunceble
-# ****************************************************************
-
-# **********************
-# Setting date variables
-# **********************
-
-tag=$(date '+%F %X %Z %z')
-
-# ******************
-# Set our Input File
-# ******************
-input=${TRAVIS_BUILD_DIR}/PULL_REQUESTS/domains.txt
-pyfuncebleConfigurationFileLocation=${TRAVIS_BUILD_DIR}/dev-tools/.PyFunceble.yaml
-pyfuncebleProductionConfigurationFileLocation=${TRAVIS_BUILD_DIR}/dev-tools/.PyFunceble_production.yaml
+ls -lh "/usr/bin/python"*
 
 PythonVersion () {
 if grep --quiet 'python3.8' $(find /usr/bin/python3*)
@@ -52,6 +36,24 @@ fi
 }
 PythonVersion
 
+# ****************************************************************
+# This uses the awesome PyFunceble script created by Nissar Chababy
+# Find funceble at: https://github.com/funilrys/PyFunceble
+# ****************************************************************
+
+# **********************
+# Setting date variables
+# **********************
+
+tag=$(date '+%F %X %Z %z')
+
+# ******************
+# Set our Input File
+# ******************
+input=${TRAVIS_BUILD_DIR}/PULL_REQUESTS/domains.txt
+pyfuncebleConfigurationFileLocation=${TRAVIS_BUILD_DIR}/dev-tools/.PyFunceble.yaml
+pyfuncebleProductionConfigurationFileLocation=${TRAVIS_BUILD_DIR}/dev-tools/.PyFunceble_production.yaml
+
 # **********************
 # Run PyFunceble Testing
 # **********************************************************
@@ -60,7 +62,6 @@ PythonVersion
 
 RunFunceble () {
 
-    PyFunceble=$(which PyFunceble)
     tag=$(date '+%F %X %Z %z')
     ulimit -u
     cd ${TRAVIS_BUILD_DIR}/dev-tools
@@ -73,7 +74,7 @@ RunFunceble () {
         rm "${pyfuncebleProductionConfigurationFileLocation}"
     fi
 
-    "${python3}" "$PyFunceble" --ci -q -ex --plain --idna -db -h --http --database-type mariadb -m -p 4 \
+    "${python3}" PyFunceble --ci -q -ex --plain --idna -db -h --http --database-type mariadb -m -p 4 \
         --cmd-before-end "bash ${TRAVIS_BUILD_DIR}/dev-tools/FinalCommit.sh" \
         --autosave-minutes 20 --ci-branch master --ci-distribution-branch master \
         --dns 127.0.0.1 -f ${input}
